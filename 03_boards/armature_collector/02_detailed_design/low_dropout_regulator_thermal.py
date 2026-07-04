0e85c21a57cb973022178a21180e15e585bdbb82
William Bowley <wgrantbowley@gmail.com>
2026-07-05 02:34:11 +1000

    Reorganising repo and starting README improvements

---
+++ 03_boards/armature_collector/02_detailed_design/low_dropout_regulator_thermal.py
@@ -0,0 +1,14 @@
+"""
+Filename: low_dropout_regulator_thermal.py
+
+Description:
+    Calculates the thermal via power dissipation in
+    the regulator.
+
+    Reference:
+    LD1117: "motors/armature_data_board/components/Voltage Regulator LD1117.pdf"
+"""
+
+"""
+WORK IN PROGRESS.
+"""
