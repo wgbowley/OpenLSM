b25c41336b373ea9b65a653729875f46a3ef3dcd
William Bowley <wgrantbowley@gmail.com>
2026-07-13 22:34:56 +1000

    Derived parameters & motor equations for reduced order model.

--- 01_simulation/00_reduced_order_models/src/physics/derived.py
+++ 01_simulation/00_reduced_order_models/src/physics/derived.py
@@ -1,1 +1,46 @@
-""" WORK IN PROGRESS. """
+"""
+Filename: derived.py
+
+Description:
+    Equations for derived parameters 
+    within the reduced order model of 
+    the motor.
+"""
+
+from math import pi, floor
+from builtins import float as f
+
+
+def compute_turns(length: f, thickness: f, wire_diameter: f, fill_factor: f) -> f:
+    """ 
+    Computes the number of turns while according for the insulation & stacking 
+    Assumptions: Fill factor accounts for insulation & stacking
+    """
+    slot_section = length * thickness
+    wire_section = pi * (wire_diameter / 2) ** 2
+
+    effective_area = slot_section * fill_factor
+    return floor(effective_area / wire_section)
+
+
+def compute_resistance(turns: f, mean_rad: f, wire_dia: f, resistivity: f) -> f:
+    """ 
+    Computes the slots resistance by using mean radius & conductor cross section 
+    Assumptions: Uses mean radius as an approximate for approximate turn length
+    """
+    turn_length = pi * turns * mean_rad
+    cross_section = pi * (wire_dia / 2) ** 2
+    
+    return resistivity * turn_length / cross_section
+
+
+def compute_slot_volume(turns: f, wire_dia: f, mean_radius: f) -> f:
+    """ 
+    Computes the volume of the slots using wire diameter & mean radius 
+    Assumptions: Uses mean radius as an approximate for approximate turn length
+    """
+    cross_section = pi * (wire_dia / 2) ** 2
+    total_area = turns * cross_section
+    mean_turn_length = 2 * pi * mean_radius
+
+    return total_area * mean_turn_length
--- 01_simulation/00_reduced_order_models/src/physics/motor.py
+++ 01_simulation/00_reduced_order_models/src/physics/motor.py
@@ -1,1 +1,66 @@
-""" WORK IN PROGRESS. """
+"""
+Filename: motor.py
+
+Description:
+    Dynamics equations for modelling the
+    reduced order model of the motor.
+"""
+
+from builtins import float as f
+from math import pi
+
+
+def compute_inductance(turns: f, coil_len: f, mean_radius: f, permeability: f) -> f:
+    """ Calculates the coils self-inductance independent of mutual inductance between coils """
+    area = pi * mean_radius ** 2
+    return (turns ** 2 * permeability * area) / coil_len
+
+
+def computes_inductor_voltage(
+    supply_voltage: f, current: f, resistance: f, induced_voltage: f
+) -> f:
+    """ Computes the potential difference across the inductor """
+    voltage_drop = current * resistance
+    return supply_voltage - voltage_drop + induced_voltage
+
+
+def compute_current(
+    current: f, voltage: f, inductance: f, resistance: f, time_step: f
+) -> tuple[f, f]:
+    """ 
+    Computes the current using 2nd oder ralston's method however assumes the current changes.
+    Assumptions: Assumes induced voltage term does not change (semi-implicit integration)
+    """
+    k1 = voltage / inductance
+    
+    # Updates the voltage for the next predicted frame
+    voltage = voltage - resistance * 3 / 4 * k1 * time_step
+    k2 = voltage / inductance
+    
+    di_dt = (1/3 * k1 + 2/3 * k2)
+    current += di_dt * time_step
+    return current, di_dt   
+    
+
+def compute_z_field_strength(
+    z_pos: f, z_slot: f, current: f, turns: f, slot_len: f, slot_rad: f
+) -> f:
+    """
+    Computes the field strength at a position z within or outside of the slot using the 
+    finite continuous solenoid model in axial-symmetric modelling (Z, R).
+    
+    'z_slot' is in reference to the front of the slot. Hence center is shifted back slot_len / 2
+    """
+    half_length = slot_len / 2
+    slot_center = z_pos - (z_slot - half_length)
+    
+    # Calculates the axial field components (term 1 & term 2)
+    denom1 = slot_len * (slot_rad ** 2 + (slot_center + half_length)**2) ** 0.5
+    term1 = (slot_center + half_length) / denom1
+    
+    denom2 = slot_len * (slot_rad ** 2 + (slot_center - half_length)**2) ** 0.5
+    term2 = (slot_center - half_length) / denom2
+    
+    # Calculates maximal field_strength and returns position dependent strength
+    h_term = turns * current / 2 
+    return h_term * (term1 - term2)
