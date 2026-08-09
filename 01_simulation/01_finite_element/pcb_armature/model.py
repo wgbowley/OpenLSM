a3a190f0c6ac7f7af126a072d1948c2b0c30eff4
William Bowley <wgrantbowley@gmail.com>
2026-08-09 21:14:21 +1000

    Initial parametric geometry done

--- 01_simulation/01_finite_element/pcb_armature/model.py
+++ 01_simulation/01_finite_element/pcb_armature/model.py
@@ -6,12 +6,15 @@ Description:
     motor for usage in FEM simulation
 """
 
-from pyfea import nullset, ampere
+from pyfea import nullset, ampere, mm
 from pyfea.domain.units import DynamicLoader
 
 from pyfea.domain.materials.manager import MaterialManager
 from pyfea.domain.geometry.definitions import CoordinateSystem
+from pyfea.domain.geometry.builder import Builder, VectorGeometry, Part
+from pyfea.domain.geometry.domain import Domain, BoundaryType
 
+from pyfea.solver.solver_interface import BaseSolver, MagneticSolver
 from pyfea.domain.circuits.builder import StaticCircuit, Configuration as CircuitConfig
 
 
@@ -43,6 +46,56 @@ class PCBArmatureMotor:
         self._load_materials_properties()
         self._compute_derived_parameters()
 
+    def construct_domain(self, solver: BaseSolver) -> tuple[Domain, list[Part]]:
+        """ Constructs the domain based on solver physics """
+        solver_interfaces = solver.__class__.__bases__
+
+        for solver in solver_interfaces:
+            if solver == MagneticSolver:
+                return None # Temp.
+
+        msg = f"{solver_interfaces!r} is not supported by {self.__class__.__name__!r}"
+        raise ModelError("PCBArmatureMotor.construct_domain()", msg)
+
+    def build_armature(self) -> list[VectorGeometry]:
+        """ Builds the slots within the armature """
+        slots = []
+
+        number_layers = self.sub_slots * self.number_slots * self.pcb_layers
+        for index in range(0, int(number_layers.value)):
+            offset = - self.armature_length / 2
+            bottom_left = offset + index * (self.pcb_trace_thickness + self.pcb_substrate)
+
+            slot_shape = Builder.rectangle(
+                (self.winding_inner_radius, bottom_left),
+                self.winding_thickness, self.pcb_trace_thickness
+            )
+            slots.append(slot_shape)
+
+        return slots
+
+    def build_stator(self) -> list[VectorGeometry]:
+        """ Builds the poles within the stator """
+        dipoles = []
+
+        for index in range(0, int(self.number_dipoles.value)):
+            offset = - self.stator_axi_len / 2
+            bottom_left = offset + index * self.dipole_pitch + self.dipole_pitch / 2
+
+            dipoles_shape = Builder.rectangle(
+                (0 * mm, bottom_left), self.dipole_radius, self.dipole_pitch
+            )
+            dipoles.append(dipoles_shape)
+
+        return dipoles
+
+    def build_boundary(self) -> VectorGeometry:
+        """ Builds the boundary shape via tube length with 20% margin axially and 100% radially """
+        max_length = 1.2 * self.params.stator.tube.axi_length
+        max_radius = 2 * self.winding_outer_radius
+
+        return Builder.rectangle((0 * mm, -max_length / 2), max_radius, max_length)
+
     def _load_materials_properties(self) -> None:
         """ Loads the material properties into class data """
         manager = MaterialManager()
@@ -58,11 +111,14 @@ class PCBArmatureMotor:
 
     def _compute_derived_parameters(self) -> None:
         """ Computes derived parameters from static parameters """
-        self.pole_pitch = self.params.stator.dipole.axi_length
-        self.number_poles = self.params.stator.tube.axi_length / self.pole_pitch
+        self.stator_axi_len = self.params.stator.tube.axi_length
+        self.dipole_pitch = self.params.stator.dipole.axi_length
+
+        # Computes the number of dipoles within the stator
+        self.number_dipoles = self.stator_axi_len / self.dipole_pitch
 
         # Computes the slot pitch from pole pitch
-        self.slot_pitch = self.pole_pitch / 3
+        self.slot_pitch = self.dipole_pitch / 3
 
         # Computes the number of pcb's within each slot
         self.pcb_layers = self.params.armature.sub_slot.axi_layers
@@ -73,11 +129,14 @@ class PCBArmatureMotor:
         self.sub_slots = self.slot_pitch // self.pcb_axi_thickness
 
         # Computes the armature length
-        self.armature_length = self.slot_pitch * self.params.armature.core.number_slots
+        self.number_slots = self.params.armature.core.number_slots
+        self.armature_length = self.slot_pitch * self.number_slots
 
         # Computes the radial placement of components
-        self.pole_radius = self.params.stator.dipole.rad_thickness
-        self.tube_radius = self.pole_radius + self.params.stator.tube.rad_thickness
+        self.dipole_radius = self.params.stator.dipole.rad_thickness
+        self.tube_radius = self.dipole_radius + self.params.stator.tube.rad_thickness
         self.arm_inner_radius = self.tube_radius + self.params.armature.core.rad_clearance
         self.winding_inner_radius = self.arm_inner_radius + self.params.armature.core.pcb_edge
-        self.winding_outer_radius = self.winding_inner_radius + self.params.armature.slot.rad_thickness
+
+        self.winding_thickness = self.params.armature.slot.rad_thickness
+        self.winding_outer_radius = self.winding_inner_radius + self.winding_thickness
