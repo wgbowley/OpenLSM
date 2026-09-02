f199a065c4642948d38688a98eb94d23a0ebbc31
William Bowley <wgrantbowley@gmail.com>
2026-09-02 23:21:01 +1000

    Nearly finished the schematic for driver board

---
+++ 03_boards/00_bridge_driver/02_detailed_design/voltage_follower.py
@@ -0,0 +1,11 @@
+"""
+Filename: voltage_follower.py
+
+Description:
+    Calculates the voltage driver 
+    network for the reference.
+"""
+
+# *(work in progress) *
+# Basically just calculates the resistor size and tolerance
+# for a good output based on the ina214A2 outputs.
