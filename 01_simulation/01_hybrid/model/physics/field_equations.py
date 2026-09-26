88290d695e85c0eba45c96e6f0b09e61b1901345
William Bowley <wgrantbowley@gmail.com>
2026-09-26 22:12:24 +1000

    Biot sum integrand

--- 01_simulation/01_hybrid/model/physics/field_equations.py
+++ 01_simulation/01_hybrid/model/physics/field_equations.py
@@ -26,3 +26,19 @@ def derivative_helix(t: np.ndarray, radius: float, pitch: float, dt: float) -> n
 
     # Creates an array of [(dx1, dy1, dz1), ... , (dxn, dyn, dzn)]
     return np.stack([dx, dy, dz], axis=1)
+
+
+def biot_sum_integrand(r_eval: np.ndarray, r_wire: np.ndarray, dl: np.ndarray) -> np.ndarray:
+    """ Biot savart sum of the integrand """
+    r_vec = np.expand_dims(r_eval, axis=2) - r_wire
+
+    # Calculates magnitude of r_vec and forces safe flooring
+    distance = np.sqrt(np.sum(r_vec**2, axis=-1))
+    flooring = np.maximum(distance, 1e-10)
+
+    # Expands dl segment and cross products the result
+    dl_expanded = np.expand_dims(np.expand_dims(dl, axis=0), axis=0)
+    cross = np.cross(dl_expanded, r_vec, axis=-1)
+
+    # Resulting integrand
+    return cross / np.expand_dims(flooring ** 3, axis=-1)
